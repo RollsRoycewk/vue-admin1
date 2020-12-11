9852ec0201c55cf5f8af16b7a87a368383d29e7c
RollsRoycewk <50765560@qq.com>
2020-12-11 23:11:10 +0800

    feat:(trademark)定义api接口文档

---
+++ src/api/product/trademark.js
@@ -0,0 +1,42 @@
+import request from "@/utils/request";
+
+const api_name = "/admin/product/baseTrademark";
+
+export default {
+  /* 获取所有商品列表 */
+  getPageList() {
+    return request({
+      url: `${api_name}/getTrademarkList`,
+      method: "GET"
+    });
+  },
+  /* 增加商品 */
+  addPageList(data) {
+    return request({
+      url: `${api_name}/save`,
+      method: "POST",
+      data
+    });
+  },
+  /* 删除商品 */
+  delPageList(id) {
+    return request({
+      url: `${api_name}/remove/${id}`,
+      method: "DELETE"
+    });
+  },
+  /* 更新商品 */
+  updataPageList() {
+    return request({
+      url: `${api_name}/update`,
+      method: "PUT"
+    });
+  },
+  /* 获取所有商品列表 */
+  getPageList() {
+    return request({
+      url: `${api_name}/getTrademarkList`,
+      method: "GET"
+    });
+  }
+};
