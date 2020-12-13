e5171d56e3e3d0c0b3a29aa22317eab481e8f268
RollsRoycewk <50765560@qq.com>
2020-12-13 18:32:30 +0800

    feat:(list)属性页面基本样式完成

---
+++ src/views/product/attr/Category.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- 分类选择列表 -->
+  <el-card class="box-card">
+    <el-form :inline="true" :model="category" class="demo-form-inline">
+      <el-form-item label="一级分类">
+        <el-select v-model="category1Id" placeholder="请选择">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="二级分类">
+        <el-select v-model="category2Id" placeholder="请选择">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="三级分类">
+        <el-select v-model="category3Id" placeholder="请选择">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  data() {
+    return {
+      category: {
+        category1Id: "",
+        category2Id: "",
+        category3Id: "",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/product/attr/list.vue
+++ src/views/product/attr/list.vue
@@ -1,11 +1,43 @@
 <template>
-<div>
-  平台属性
-</div>
+  <div>
+    <Category></Category>
+    <!-- 属性 -->
+    <el-card class="box-card" style="margin: 20px 0">
+      <el-button type="primary" disabled>
+        <i class="el-icon-plus"></i>
+        <span>添加属性</span>
+      </el-button>
+      <el-table :data="attrsData" border style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="序号"
+          width="80"
+          align="center"
+          type="index"
+        >
+        </el-table-column>
+        <el-table-column prop="name" label="属性值名称" width="150">
+        </el-table-column>
+        <el-table-column prop="address" label="属性值列表"> </el-table-column>
+        <el-table-column prop="address" label="操作" width="150">
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import Category from "./Category";
+
 export default {
-  name: 'AttrList',
-}
+  name: "AttrList",
+  data() {
+    return {
+      attrsData: [],
+    };
+  },
+  components: {
+    Category,
+  },
+};
 </script>
